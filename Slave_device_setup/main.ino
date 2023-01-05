c5af9dcae70a929b53d2f2f5ae69aaf212d51e38
Khali04 <khali04@live.com>
2023-01-05 17:14:58 -0500

    je travail sur fichier principal

---
+++ Slave_device_setup/main.ino
@@ -0,0 +1,54 @@
+    
+const int EnableL = 5;
+const int HighL = 6;       // LEFT SIDE MOTOR
+const int LowL =7;
+
+const int EnableR = 10;
+const int HighR = 8;       //RIGHT SIDE MOTOR
+const int LowR =9;
+
+
+void setup() {
+
+}
+
+
+void Left1()
+{
+ 
+  
+}
+
+void Left2()
+{
+ 
+  
+}
+
+void Left3()
+{
+  
+}
+
+void Right1()
+{
+  
+}
+
+void Right2()
+{
+  
+  
+}
+
+void Right3()
+{
+  
+  
+}
+
+
+void loop() 
+{
+  
+}
